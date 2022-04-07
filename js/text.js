7f00b815cbdb7a67f338f52b1c1da43f8a841171
Mariia <masya.kim@mail.ru>
2022-04-07 21:16:36 +0300

    edit text.js

--- js/text.js
+++ js/text.js
@@ -11,7 +11,17 @@
     Blotter.LiquidDistortMaterial.prototype,
     (function () {
       function _mainImageSrc() {
-        var mainImageSrc = [Blotter.Assets.Shaders.Noise3D].join("\n");
+        var mainImageSrc = [
+          Blotter.Assets.Shaders.Noise3D,
+          "void mainImage( out vec4 mainImage, in vec2 fragCoord )",
+          "{",
+          "    // Setup ========================================================================",
+          "    vec2 uv = fragCoord.xy / uResolution.xy;",
+          "    float z = uSeed + uGlobalTime * uSpeed;",
+          "    uv += snoise(vec3(uv, z)) * uVolatility;",
+          "    mainImage = textTexture(uv);",
+          "}",
+        ].join("\n");
 
         return mainImageSrc;
       }
